9e43685e4662937da18bcba933983ed9a342987c
vietthang207 <vietthang207@gmail.com>
2016-07-05 23:46:22 +0800

    bitset train

---
+++ uva/uva594.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+int main(){
+    int n=-1 ;
+    int count=1;
+    cin>>n;
+    while (n!=-1){
+
+        bitset<32> bs(n);
+        bitset<32> clone;
+        for (int i=0; i<8; i++){
+            clone[i] = bs[24+i];
+            clone[i+8] = bs[16+i];
+            clone[i+16] = bs[8+i];
+            clone[i+24] = bs[i];
+        }
+
+        printf("%d converts to %d\n", n, clone);
+
+        count ++;
+        n=-1;
+        cin>>n;
+    }
+}
