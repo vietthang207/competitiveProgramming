7018ff0ba527f939c2f08b4c693a86da2700514f
vietthang207 <vietthang207@gmail.com>
2016-07-06 01:41:06 +0800

    fix input reading bug -1 or eof not word with test 1590323135

--- uva/uva594.cpp
+++ uva/uva594.cpp
@@ -9,9 +9,7 @@ typedef vector<pii> vii;
 int main(){
     int n=-1 ;
     int count=1;
-    cin>>n;
-    while (n!=-1){
-
+    while (cin>>n){
         bitset<32> bs(n);
         bitset<32> clone;
         for (int i=0; i<8; i++){
@@ -23,8 +21,5 @@ int main(){
 
         printf("%d converts to %d\n", n, clone);
 
-        count ++;
-        n=-1;
-        cin>>n;
     }
 }
