1eaa90364348b17883a491a0cb993bd951726d49
vietthang207 <vietthang207@gmail.com>
2016-08-08 22:09:40 +0700

    I can stand here and watch you BURN

---
+++ uva/graph/uva10557.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef map<int, int> mii;
+
+const double EP=1e-6;
+const ll MAX31 = 2147483647;
+const ll MIN31 = -2147483648;
+
+int n;
+vi al[110];
+int ene[110];
+int sp[110];
+
+int main(){
+	int coun=1;
+	while (cin>>n){
+		if (n==-1) break;
+		for (int i=0; i<n; i++){
+			sp[i] = MAX31;
+			al[i].clear();
+			cin>>ene[i];
+			ene[i] = -ene[i];
+			int m; cin>>m;
+			for (int j=0; j<m; j++){
+				int tmp; cin>>tmp;
+				tmp--;
+				al[i].push_back(tmp);
+			}
+		}
+
+		sp[0] = 0;
+		for (int k=0; k<n-1; k++){}
+			for (int i=0; i<n; i++){
+				for (int j=0; j<al[i].size(); j++){
+					sp[j] = min(sp[j], ene[j] + sp[i]);
+				}
+			}
+		}
+		set<int> loop;
+		for (int i=0; i<n; i++){
+			for (int j=0; j<al[i].size(); j++){
+				if (sp[j]>sp[i]+ene[j]) {
+					loop.insert(j);
+				}
+			}
+		}
+
+		coun ++;
+	}
+}
---
+++ uva/graph/uva12442.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef map<int, int> mii;
+
+const double EP=1e-6;
+const ll MAX31 = 2147483647;
+const ll MIN31 = -2147483648;
+
+int T, n;
+vi al[55000];
+int memo[55000];
+bool visited[55000];
+stack<int> st;
+
+int dfs(int v){
+	// cout<<v<<endl;
+	if (visited[v]) return memo[v];
+	visited[v] = true;
+	int size = al[v].size();
+	if (size==0) return memo[v] = 1;
+	if (!visited[al[v][0]]) return memo[v] = 1 + dfs(al[v][0]);
+	return memo[v] = 1 + memo[al[v][0]];
+}
+
+int main(){
+	cin>>T;
+	for (int t=0; t<T; t++){
+		for (int i=0; i<55000; i++) al[i].clear();
+		memset(memo, -1, sizeof memo);
+		memset(visited, false, sizeof visited);
+		cin>>n;
+		for (int i=0; i<n; i++){
+			int v1,v2;
+			cin>>v1>>v2;
+			v1--; v2--;
+			al[v1].push_back(v2);
+		}
+
+		int res, maxi=MIN31;
+		for (int i=0; i<n; i++){
+			if (memo[i]==-1) dfs(i);
+			cout<<memo[i]<<endl;
+			if (memo[i]>maxi){
+				maxi = memo[i];
+				res = i;
+			}
+		}
+		printf("Case %d: %d\n", t+1, res+1);
+
+	}
+}
