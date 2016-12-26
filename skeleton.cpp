3c3bdae25fb1164a8fc44777499c8c9e393793cc
GaryDinh <vietthang207@gmail.com>
2016-12-26 13:12:22 +0800

    fix

--- skeleton.cpp
+++ skeleton.cpp
@@ -5,12 +5,11 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<pii> vii;
-typedef map<int, int> mii;
 
 const double EPS=1e-9;
 const ll MAX31 = 2147483647;
 const ll MIN31 = -2147483648;
 
 int main(){
-	
+
 }
