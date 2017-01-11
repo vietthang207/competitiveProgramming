6ef512da60fc95a6c54189509a26de6de03332ce
Thang Dinh <Thang Dinh>
2017-01-12 01:06:50 +0800

    change skeleton

--- skeleton.cpp
+++ skeleton.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<pii> vii;
-typedef map<int, int> mii;
+typedef unordered_map<int, int> mii;
 
 const double EPS=1e-9;
 const ll MAX31 = 2147483647;
@@ -14,3 +14,10 @@ const ll MIN31 = -2147483648;
 int main(){
 	ios::sync_with_stdio(0);
 }
+
+
+
+
+
+
+
